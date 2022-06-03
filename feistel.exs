9b84863359f30a4400de0315ef6f242f2f448c0d
Rodrigo Márquez <a01022943@itesm.mx>
2022-06-02 19:59:41 -0500

    xor
    Co-authored-by: Vicentesve <Vicentesve@users.noreply.github.com>

--- feistel.exs
+++ feistel.exs
@@ -2,11 +2,13 @@
 # Rodrigo Márquez - A01022943
 # Vicente Santamaría - A01421801
 
+import Bitwise
+
 defmodule Feistel do
   @doc """
   Función que encripta el archivo con la llave del usuario
   """
-  def encrypt(file) do
+  def encrypt(file, key) do
     file
     # Leer el archivo
     |> File.stream!()
@@ -17,36 +19,55 @@ defmodule Feistel do
     # Convierte cada tupla en una lista
     |> Enum.map(&Tuple.to_list(&1))
     # Paralelizar?
-    |> Enum.map(&splitMsg(&1))
+    |> Enum.map(&splitMsg(&1, to_charlist(key)))
 
     # |> encriptar(&1) # Encripta cada renglón (sublista)
 
     IO.puts("encrypt ok")
   end
 
-  defp splitMsg(line) do
+  defp splitMsg(line, k) do
     l = Enum.at(line, 0)
     r = Enum.at(line, 1)
 
     if length(l) === length(r) do
-      IO.puts("impar")
-      # Correr feistel así
-      feistelRound(l, r)
-    else
       IO.puts("par")
+      # Correr feistel así
+      feistelRound(l, r, k)
+    else
+      IO.puts("impar")
       # Añadir caracter a r y correr feistel
-      r = 'X' ++ r
-      feistelRound(l, r)
+      feistelRound(l, '0' ++ r, k)
     end
   end
 
-  defp feistelRound(l0, r0) do
-    IO.puts("left")
-    IO.puts(length(l0))
-    IO.puts(l0)
-    IO.puts("right")
-    IO.puts(length(r0))
-    IO.puts(r0)
+  defp feistelRound(l0, r0, llave) do
+    # IO.puts("left")
+    # IO.puts(length(l0))
+    # IO.puts(l0)
+    # IO.puts("right")
+    # IO.puts(length(r0))
+    # IO.puts(r0)
+    # IO.puts("llave")
+    # IO.puts(length(llave))
+
+    # for n <- [1, 2, 3, 4, 5], do: IO.puts(n * 2)
+
+    Enum.each(0..(length(r0) - 1), fn x ->
+      bxor(Enum.at(r0, x), Enum.at(llave, rem(x, length(llave))))
+    end)
+
+    # for (i,i=<r0.length,i++){
+    #   xor(r0[i], key[i mod key.length])
+    #                         10
+    #                         11 mod 10 = 1
+    #                         msg mide 20
+    #                         key mide 10
+    #                         i 1-10
+    #                         i 11-20 mg i,  key 11 mod 10 = 1 12mod10 2
+    # }
+
+    # bxor(a,b)
   end
 
   @doc """
@@ -57,4 +78,4 @@ defmodule Feistel do
   end
 end
 
-Feistel.encrypt("prueba.txt")
+Feistel.encrypt("prueba.txt", "llave")
